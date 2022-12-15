3a554bd9fba0a30898379380e468eaa6f98f565b
Koverin <110396064+Koverin@users.noreply.github.com>
2022-12-15 16:50:06 +0200

    update gallery 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,38 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-const options = {
-  /*
-   * Prevents the lightbox from closing when clicking its background.
-   */
-  closable: true,
-  /*
-   * One or more space separated classes to be added to the basicLightbox element.
-   */
-  className: "",
-  /*
-   * Function that gets executed before the lightbox will be shown.
-   * Returning false will prevent the lightbox from showing.
-   */
-  onShow: (instance) => {
-    console.log("что это");
-  },
+const galleryBox = document.querySelector(".gallery");
+const cardsMarkup = createCardsMarkup(galleryItems);
 
-  /*
-   * Function that gets executed before the lightbox closes.
-   * Returning false will prevent the lightbox from closing.
-   */
-  onClose: (instance) => {
-    console.log("это то");
-  },
-};
+galleryBox.insertAdjacentHTML("beforeend", cardsMarkup);
+// console.log(galleryBox);
 
-console.log(basicLightbox);
-const instance = basicLightbox.create(
-  `<img src = "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg" alt = "Aerial Beach View">`,
-  options
-);
+galleryBox.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log("target", event.target);
+  console.log("currentTarget", event.currentTarget);
 
-instance.show();
+  const imageBigSize = event.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`<img src = "${imageBigSize}">`);
+
+  instance.show();
+});
+
+function createCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class = "gallery__link" href = "${original}">
+      <img class = "gallery__image" src = "${preview}" 
+      data-source = "${original}" alt = "${description}"></a>`
+    )
+    .join("");
+}
+
+// console.log(cardsMarkup);
+
+// console.log(basicLightbox);
